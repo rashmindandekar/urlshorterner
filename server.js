5dce1f50c91e25ba489a62fa41219e3e6d32eb1a
rdhotmail <>
2021-06-07 04:03:53 +0000

    my code

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns=require('dns');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+
+var myurls=[];
+const shortnums=[];
+for(var i=0;i<5000;i++){
+  shortnums[i]=i;
+}
+
+function getlongfromshort(urls,shortin){
+  for(var i=0;i<urls.length;i++){
+    //console.log(shortin);
+    //console.log(urls[i].short_url);
+    if(urls[i].short_url ==  shortin) {
+      //console.log(urls[i].original_url);
+      return urls[i].original_url;
+      }
+  }
+  return false;
+}
+
+function getshortfromlong(urls,longin){
+  for (var i=0;i<urls.length;i++){
+    if (urls[i].long == longin) {return urls[i].short;}
+  }
+  return false;
+}
 
 app.use(cors());
 
@@ -19,6 +46,33 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+function inputurl(req,res){
+  dns.lookup(req.body.url,(error)=>{
+    if (error){
+      return res.json({error:'invalid url'});
+    }
+    else{
+      myurls.push({original_url:req.body.url, short_url:shortnums[myurls.length]});
+      console.log(myurls[myurls.length-1]);
+      return res.json(myurls[myurls.length-1]);
+    }
+  });
+}
+
+
+app.post('/api/shorturl',inputurl);
+app.get('/api/shorturl/:shorturl', (req,res)=>{
+  var myshorturl=req.params.shorturl;
+  var redirecturl=getlongfromshort(myurls,myshorturl);
+  console.log(redirecturl);
+  //res.json({out:myshorturl});
+  res.redirect(redirecturl);
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
